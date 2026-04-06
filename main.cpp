73567e5a28d39241c2ea2a2cf90b8b93c4dd021b
Ruslan <ruslanvl2008@gmail.com>
2026-04-06 17:20:01 +1000

    библиотеки и константы

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Библиотеки
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <iostream>
+#include <cmath>
+
+// Константы
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int BALL_RADIUS = 20;
